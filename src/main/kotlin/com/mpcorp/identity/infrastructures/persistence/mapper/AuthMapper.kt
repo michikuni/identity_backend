1e66c22c65ec4e23ea42a81045c91d55060be52d
Minh Phuong Dang <minhphuonglcby@email.com>
2026-03-16 16:42:57 +0700

    Feat: Create helper mapper data

---
+++ src/main/kotlin/com/mpcorp/identity/infrastructures/persistence/mapper/AuthMapper.kt
@@ -0,0 +1,23 @@
+package com.mpcorp.identity.infrastructures.persistence.mapper
+
+import com.mpcorp.identity.domain.entity.AuthEntity
+import com.mpcorp.identity.infrastructures.persistence.jpa_entity.AuthJpaEntity
+
+fun AuthJpaEntity.toDomainEntity() : AuthEntity {
+    return AuthEntity(
+        id = id,
+        email = email,
+        password = password,
+        phone = phone,
+        role = role,
+    )
+}
+
+fun AuthEntity.toPersistentEntity() : AuthJpaEntity {
+    return AuthJpaEntity(
+        email = email,
+        password = password,
+        phone = phone,
+        role = role,
+    )
+}
