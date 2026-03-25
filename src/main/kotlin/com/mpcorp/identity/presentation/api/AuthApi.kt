9b94fb6b837bf0a77d2c96a714ee2490701ccb79
Minh Phuong Dang <minhphuonglcby@gmail.com>
2026-03-25 23:12:38 +0700

    Update: Implement payroll, profile repository

--- src/main/kotlin/com/mpcorp/identity/presentation/api/AuthApi.kt
+++ src/main/kotlin/com/mpcorp/identity/presentation/api/AuthApi.kt
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 
-@RequestMapping("/api/auth")
+@RequestMapping("/api/v1/auth")
 interface AuthApi {
     @PostMapping("/sign-in")
     fun signIn(@RequestBody signInRequest: SignInRequest): SignInResponse
